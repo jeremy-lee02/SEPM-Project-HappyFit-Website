e7545e9e9c69356915e298b0818cc515d9a73719
Trung Tín <s3752975@rmit.edu.vn>
2022-07-20 10:32:33 +0700

    update body homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,9 @@ function Home() {
     <>
       <Header />
       {/* Content */}
-      <div>
-        <div className='flex flex-col md:flex-row flex-auto pt-5'>
+      <div className='bg-black'>
+        {/* Content 1 */}
+        <div className='flex flex-col md:flex-row flex-auto pt-5 md:hover:drop-shadow-lg'>
           {/* Left  */}
           <div className='flex md:flex-initial'>
               <img src={require('../images/pic1.png')} alt='pic 1' className='pl-11 w-full'/>
@@ -26,6 +27,21 @@ function Home() {
             <button className='bg-blue hover:bg-blue-700 hover:drop-shadow-lg focus:ring focus:ring-violet-300 text-white font-bold py-2 px-4 rounded-md' onClick={changeRoute}>Start your workout!</button>
           </div>
         </div>
+        {/* Content 2 */}
+        <div className='pt-10 flex flex-col-reverse md:flex-row flex-auto pb-8 flex-row-reverse md:hover:drop-shadow-lg rounded-md'>
+          {/* Left */}
+          <div className='flex w-full md:pl-11 text-left pt-5 item-center justify-center italic flex-col'>
+          <p className='pb-3 text-xl'>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            </p>
+          </div>
+          {/* Right */}
+          <div className='flex md:flex-initial'>
+            <img src={require('../images/pic2.png')} alt='pic 1' className='md:pr-14 w-full pl-14'/>
+          </div>
+
+        </div>
+
       </div>
     </>
   )
