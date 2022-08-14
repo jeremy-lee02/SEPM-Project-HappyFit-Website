6f928dd8ed9a0d213acd905d620ad18de0298e0c
AlOmEx <omega.ex1234@gmail.com>
2022-08-14 17:30:26 +0700

    created login and register UI

---
+++ src/components/Login/InputLogin.js
@@ -0,0 +1,16 @@
+import React from "react";
+function InputLogin({type, name, placeholder, onChange, value}){
+    return (
+        <>
+        <input 
+          type={type} 
+          className="rounded-lg mt-2 p-2" 
+          name={name} 
+          value = {value}
+          placeholder={placeholder}
+          onChange = {onChange}
+          />
+        </>
+      )
+}
+export default InputLogin
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
+import InputLogin from '../components/Login/InputLogin';
+import ButtonLogin from '../components/Login/ButtonLogin';
 
 function Login() {
     const navigate = useNavigate();
@@ -8,9 +10,24 @@ function Login() {
       navigate(path)
     }
   return (
-    <div className='mt-28 h-screen'>
-      <h1>Login</h1>
-    <button onClick={changeRoute}>Does not have an acoount?</button>
+    <div className=' w-full h-screen bg-gray-800'>
+      <div className=' items-center justify-center'>
+        <div className='mt-24 text-white'>
+        <h1 className='justify-center pt-5'>Login</h1>
+        </div>
+        <form action='' className='max-w-[550px] w-full mx-auto p-20 px-8 rounded-lg  bg-gray-900 mt-10'>
+            <div className='flex flex-col py-2'>
+              <InputLogin type={'text'} name={'loginUsername'} placeholder={'Email'}/>
+            </div>
+            <div className='flex flex-col py-2'>
+              <InputLogin type={'password'} name={'loginPassword'} placeholder={'Password'}/>
+            </div>
+            <ButtonLogin onClick={'submit'} text={'Sign In'}/>
+            <div>
+            <button className='text-white hover:text-blue p-2' onClick={changeRoute}>Doesn't have an account?</button>
+            </div>
+        </form>
+      </div>
     </div>
   )
 }
