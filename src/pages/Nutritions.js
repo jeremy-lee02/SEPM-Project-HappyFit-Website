93173a9fda8f321d470a22d101d1ba87458c360f
Trung Tín <s3752975@rmit.edu.vn>
2022-08-15 17:43:23 +0700

    update nutrition api

--- src/pages/Nutritions.js
+++ src/pages/Nutritions.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Modal from '../components/Modal/Modal'
 import Skeleton from '../components/Skeleton/Skeleton'
 import Dummy_Data from '../pages/data.json'
@@ -14,35 +14,50 @@ function Nutritions() {
   const [modalData, setModalData] = useState([])
   const [loading, setLoading] = useState(true)
   const [classProps, setClassProps] = useState("")
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    setLoading(true)
+    fetch('https://happy-fit-api.herokuapp.com/nutrition')
+    .then(res => res.json())
+    .then(json => setData(json))
+    setLoading(false)
+  }, [])
 
-  
 
   return (
     <div className="">
     <h1 className="mt-24 justify-center pt-3">Nutritions</h1>
     <Search/>
     <div className= {`grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-16 gap-10 ${classProps}`}>
-      {!loading?Dummy_Data.map((item) => (
+      {!loading?data.map((item) => (
       <div className="cursor-pointer hover:scale-105 tranform transition duration-300 ease-out" key={item.id} onClick ={()=>{
         setShowModal(prev => !prev)
         setModalData(item);
       }}>
        <div className="card-top">
         <div className="relative">
-         <img src={item.img} layout="fill" className='w-full' alt={item.title}/>
+         <img src={require('../images/giphy.gif')} layout="fill" className='w-3/4' alt={item.name}/>
          </div>
-          <h2 className="text-2xl mt-3 underline">{item.title}</h2>
+          <h2 className="text-2xl mt-3 underline">{item.name}</h2>
           <h3 className='font-bold inline-flex pb-1 pt-1'>Description: </h3>
           <span> {item.description}</span>
           <br />
-          <h3 className='font-bold inline-flex pt-1 pb-1'>Ingredient: </h3>
-          <span> {item.tips}</span>
+          <h3 className='font-bold inline-flex pt-1 pb-1'>Nutrition: </h3>
+          <span> {item.nutrition}</span>
         </div>
       </div>
       )): <Skeleton/>}
       <Skeleton />
       </div>
-        <Modal showModal={showModal} setShowModal = {setShowModal} img = {modalData.img} title = {modalData.title} />
+        <Modal 
+        showModal={showModal} 
+        setShowModal = {setShowModal} 
+        img = {require('../images/giphy.gif')} 
+        title = {modalData.name}
+        title1 = {'Ingredients: '} 
+        tech = {modalData.ingredient}
+        title2= {'Intruction:'}
+        instruct={modalData.instruction}/>
       </div>
 )}
 
