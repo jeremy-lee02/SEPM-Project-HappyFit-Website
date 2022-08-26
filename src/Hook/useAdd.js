172b0c5d3c22e74e1c5d986b568b03f1f503099c
Trung Tín <s3752975@rmit.edu.vn>
2022-08-26 21:36:07 +0700

    update error

--- src/Hook/useAdd.js
+++ src/Hook/useAdd.js
@@ -7,20 +7,21 @@ export default function useAdd(added, date) {
   const addExercise= async() =>{
     const user = JSON.parse(localStorage.getItem('profile'))
     const schedule = JSON.parse(localStorage.getItem('schedule'))
-    
-    const {data} = await axios.patch(
-      `https://happy-fit-api.herokuapp.com/schedule/${user.id}/${schedule._id}`,
-      {id: added._id,
-      day:date},
-      {headers:{
-        Authorization: `Bearer ${user.token}`
-      }}
-    )
-    console.log(data)
-    console.log(user)
-    console.log(schedule)
-    localStorage.removeItem('schedule')
-    localStorage.setItem('schedule',JSON.stringify(data))
+    if(user&&schedule){
+      const {data} = await axios.patch(
+        `https://happy-fit-api.herokuapp.com/schedule/${user.id}/${schedule._id}`,
+        {id: added._id,
+        day:date},
+        {headers:{
+          Authorization: `Bearer ${user.token}`
+        }}
+      )
+      console.log(data)
+      console.log(user)
+      console.log(schedule)
+      localStorage.removeItem('schedule')
+      localStorage.setItem('schedule',JSON.stringify(data))
+    }
   }
     useEffect(()=>{
         addExercise();
